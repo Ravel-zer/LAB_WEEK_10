cc66f4f4d214ab789d3d384f72af5d2e8c2a266d
Ravel <110222646+Ravel-zer@users.noreply.github.com>
2025-11-16 00:44:34 +0700

    Commit No. 4: Complete Bonus Assignment (Add Update Timestamp)

--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -3,28 +3,31 @@ package com.example.lab_week_10
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
 import com.example.lab_week_10.database.Total
 import com.example.lab_week_10.database.TotalDatabase
+import com.example.lab_week_10.database.TotalObject
 import com.example.lab_week_10.viewmodels.TotalViewModel
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
 
     private val db by lazy { prepareDatabase() }
-
     private val viewModel by lazy { ViewModelProvider(this)[TotalViewModel::class.java] }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         initializeValueFromDatabase()
         prepareViewModel()
     }
 
     private fun updateText(total: Int) {
-        findViewById<TextView>(R.id.text_total)?.text = getString(R.string.text_total, total)
+        findViewById<TextView>(R.id.text_total).text = getString(R.string.text_total, total)
     }
 
     private fun prepareViewModel() {
@@ -37,26 +40,50 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     private fun prepareDatabase(): TotalDatabase {
         return Room.databaseBuilder(
             applicationContext,
             TotalDatabase::class.java,
             "total-database"
-        ).allowMainThreadQueries().build()
+        ).fallbackToDestructiveMigration().allowMainThreadQueries().build()
     }
 
     private fun initializeValueFromDatabase() {
-        val total = db.totalDao().getTotal(ID)
-        if (total.isEmpty()) {
-            db.totalDao().insert(Total(id = 1, total = 0))
+        val totalList = db.totalDao().getTotal(ID)
+        if (totalList.isEmpty()) {
+            db.totalDao().insert(
+                Total(
+                    id = 1,
+                    total = TotalObject(0, Date().toString())
+                )
+            )
         } else {
-            viewModel.setTotal(total.first().total)
+            viewModel.setTotal(totalList.first().total.value)
         }
     }
 
     override fun onPause() {
         super.onPause()
-        db.totalDao().update(Total(ID, viewModel.total.value ?: 0))
+        val currentDate = Date().toString()
+        db.totalDao().update(
+            Total(
+                ID,
+                TotalObject(
+                    viewModel.total.value ?: 0,
+                    currentDate
+                )
+            )
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val totalList = db.totalDao().getTotal(ID)
+        if (totalList.isNotEmpty()) {
+            val lastDate = totalList.first().total.date
+            Toast.makeText(this, "Last updated: $lastDate", Toast.LENGTH_LONG).show()
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/lab_week_10/database/TotalDao.kt
+++ app/src/main/java/com/example/lab_week_10/database/TotalDao.kt
@@ -5,12 +5,12 @@ import androidx.room.*
 @Dao
 interface TotalDao {
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(total: Total)
-    @Update
-    fun update(total: Total)
-    @Delete
-    fun delete(total: Total)
     @Query("SELECT * FROM total WHERE id = :id")
     fun getTotal(id: Long): List<Total>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(total: Total)
+
+    @Update
+    fun update(total: Total)
 }
