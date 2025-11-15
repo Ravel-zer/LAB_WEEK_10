576326892789c977ed3199c6ef0272ce8e87ad10
Ravel <110222646+Ravel-zer@users.noreply.github.com>
2025-11-16 00:22:03 +0700

    Commit No. 2: Implement LiveData to Sync Activity and Fragment UI

--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.lab_week_10
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.lab_week_10.viewmodels.TotalViewModel
@@ -20,15 +19,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateText(total: Int) {
-        findViewById<TextView>(R.id.text_total).text = getString(R.string.text_total, total)
+        findViewById<TextView>(R.id.text_total)?.text = getString(R.string.text_total, total)
     }
 
     private fun prepareViewModel() {
-
-        viewModel.total.observe(this) { total ->
-            updateText(total)
-        }
-
+        viewModel.total.observe(this, {
+            updateText(it)
+        })
 
         findViewById<Button>(R.id.button_increment).setOnClickListener {
             viewModel.incrementTotal()
